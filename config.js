1ab0cc0073e52189bce80e0c7c3909b4cff98fdf
nalliappan.ganeshan <nalliappan.ganeshan@accenture.com>
2020-02-01 11:47:47 +0530

    Initial.

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'QBank Services',
+    env: process.env.NODE_ENV || 'development',
+      port: process.env.PORT || 8091,
+      base_url: process.env.BASE_URL || 'http://localhost:8091',
+      db: {
+          uri: process.env.MONGODB_URI || 'mongodb+srv://testGenerator:testGenerator@svsc-4hgcl.mongodb.net/test?retryWrites=true&w=majority'
+          //uri: 'mongodb://localhost:27017/admin?readPreference=primary&appname=MongoDB%20Compass&ssl=false'
+      },
+  }
---
+++ routes/users.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+const User = require('../models/User')
+
+
+router.post('/generate-token',  async (req, res, next) => {
+  try {
+    const { email, password } = req.body
+    const user = await User.findByCredentials(email, password)
+    if (!user) {
+        return res.status(401).send({error: 'Login failed! Check authentication credentials'})
+    }
+    const token = await user.generateAuthToken()
+    res.send({ user, token })
+} catch (error) {
+    res.status(400).send(error)
+}
+});
+
+router.post('/', async (req, res) => {
+  // Create a new user
+  try {
+      const user = new User(req.body)
+      await user.save()
+      const token = await user.generateAuthToken()
+      res.status(201).send({ user, token })
+  } catch (error) {
+      res.status(400).send(error)
+  }
+})
+
+module.exports = router;
